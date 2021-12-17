02385532c0169971009e59c463b2a4a004671623
dschmitz89 <danielschmitzsiegen@gmail.com>
2021-12-17 22:15:50 +0100

    Remove hash commit

--- scipybiteopt/__init__.py
+++ scipybiteopt/__init__.py
@@ -1,6 +1,5 @@
 from ._scipywrapper import biteopt, OptimizeResult, __source_version__, __source_hash__
 
 __all__ = ["biteopt",
-        "OptimizeResult",#
-        "__source_version__",
-        "__source_hash__"]
+        "OptimizeResult",
+        "__source_version__"]
